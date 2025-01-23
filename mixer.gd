6c436c5460268b9b736f40df6972b3f143960530
Kawaii Kultist <rocksmasher87@gmail.com>
2025-01-23 17:41:42 -0600

    Mixer Prototype Limits

--- mixer.gd
+++ mixer.gd
@@ -2,11 +2,22 @@ extends RigidBody2D
 
 @export var grab_velocity_expo: float = 1.5;
 
+
 var has_mouse: bool = false;
 var grabbed: bool = false;
 
+var mouse_limit_s: Vector2;
+var mouse_limit_e: Vector2;
+
 func _on_mouse_entered() -> void:  has_mouse = true;
 func _on_mouse_exited() -> void:  has_mouse = false;
+
+
+func _ready() -> void:
+	var ml_start := $Limiter/MouseLimitStart as Marker2D;
+	var ml_end := $Limiter/MouseLimitStart/MouseLimitEnd as Marker2D;
+	mouse_limit_s = ml_start.global_position as Vector2;
+	mouse_limit_e = ml_end.global_position as Vector2;
 
 
 func _input(event: InputEvent) -> void:
@@ -26,7 +37,11 @@ func _physics_process(_delta: float) -> void:
 
 
 func get_force_to_mouse(mouse_pos: Vector2) -> Vector2:
+	mouse_pos = mouse_pos.clamp(mouse_limit_s, mouse_limit_e);
+	
 	var d := global_position.direction_to(mouse_pos);
 	var f := global_position.distance_to(mouse_pos);
 	
 	return d * pow(f, grab_velocity_expo);
+
+
