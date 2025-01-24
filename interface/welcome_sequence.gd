f0a390ae9eb2977584ff1f22d91c5fa4f3fd9a42
Kawaii Kultist <rocksmasher87@gmail.com>
2025-01-24 15:11:01 -0600

    Removed repeated 'get_node()' from welcome_sequence.gd in favor of onready variables.

--- interface/welcome_sequence.gd
+++ interface/welcome_sequence.gd
@@ -1,31 +1,37 @@
 extends Control
 
 signal start_game(park_name: String)
+
 @onready var park_name = "Penguin Park"
 
+@onready var welcome_page_1 := $WelcomePage1;
+@onready var welcome_page_2 := $WelcomePage2;
+@onready var welcome_page_3 := $WelcomePage3;
+@onready var enter_park_name := $EnterParkName;
+
 func _on_next_button_1_pressed() -> void:
-	$WelcomePage1.hide()
-	$WelcomePage2.show()
+	welcome_page_1.hide()
+	welcome_page_2.show()
 
 
 func _on_next_button_2_pressed() -> void:
-	$WelcomePage2.hide()
-	$WelcomePage3.show()
+	welcome_page_2.hide()
+	welcome_page_3.show()
 
 
 func _on_next_button_3_pressed() -> void:
-	$WelcomePage3.hide()
-	$EnterParkName.show()
+	welcome_page_3.hide()
+	enter_park_name.show()
 
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	if(new_text != ""):
 		park_name = new_text
 	start_game.emit(park_name)
-	$".".hide()
+	hide()
 
 
 func _on_start_day_1_button_pressed() -> void:
 	park_name = $EnterParkName/EnterParkNameContainer/VBoxContainer/LineEdit.text
 	start_game.emit(park_name)
-	$".".hide()
+	hide()
