a57aac4ccf29964c0ffff090310d98bda761d213
Kawaii Kultist <rocksmasher87@gmail.com>
2025-01-24 17:39:56 -0600

    Attraction Data Texture Property

--- resources/attractions/attraction_data.gd
+++ resources/attractions/attraction_data.gd
@@ -1,6 +1,7 @@
 class_name AttractionData extends Resource;
 
 @export var name: StringName = &"";
+@export var texture: Texture2D;
 @export var cost_to_build: int = 100;
 @export_range(0.0, 1.0, 0.01) var attractiveness: float = 0.5;
 @export var effective_range: float = 300.0;
