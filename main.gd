5299ef6d6926f401a26424e20ec1225c19b758c2
Kawaii Kultist <rocksmasher87@gmail.com>
2025-01-24 23:35:39 -0600

    Main.gd listens for build menu selections

--- main.gd
+++ main.gd
@@ -1,4 +1,16 @@
 class_name Main extends Node2D;
 
+const ATTRACTION_DATA := {
+	0: preload("res://resources/attractions/hot_cocoa.tres"),
+	1: preload("res://resources/attractions/not_hot_cocoa.tres"),
+};
+
 
 @onready var game_gui := $CanvasLayer/GameGUI as GameGUI;
+
+func _ready() -> void:
+	game_gui.building_button_clicked.connect(_on_gui_building_button_clicked);
+
+
+func _on_gui_building_button_clicked(idx: int) -> void:
+	print("Selected: %s" % (ATTRACTION_DATA[idx] as AttractionData).name);
