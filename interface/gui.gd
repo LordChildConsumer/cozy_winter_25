e4a7a2635a65616aaabc3c5c1b15bb8893f06aac
Jamie Sullivan-Phillips <jamiesullphill@gmail.com>
2025-01-24 20:52:12 -0800

    adsjasdkashkdas

--- interface/gui.gd
+++ interface/gui.gd
@@ -3,11 +3,16 @@ extends Control
 var is_menu_hidden := true
 #slide value is 200
 
+@onready var money_label = $TopHUD/MoneyLabel
+
 signal building_button_clicked(building_index: int)
 
 @onready var build_menu_start_pos := $BuildingHUD.position as Vector2
 @export var build_menu_hide_offset = Vector2(0.0, 215.00)
 @onready var building_menu = $BuildingHUD
+
+func _ready() -> void:
+	ParkData.money_changed.connect(_on_park_data_money_changed)
 
 func _on_build_menu_button_pressed() -> void:
 	if is_menu_hidden:
@@ -30,6 +35,10 @@ func _on_build_menu_button_pressed() -> void:
 			).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT);
 		#await tween.finished;
 		is_menu_hidden = true
+
+
+func _on_park_data_money_changed(new_value: int):
+	pass
 
 
 func _on_building_button_pressed(building_index: int) -> void:
