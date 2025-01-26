b7405a3e73a9cdfed15091d37e0678ba7f4423e2
Jamie Sullivan-Phillips <jamiesullphill@gmail.com>
2025-01-25 22:39:52 -0800

    surgery

---
+++ resources/decorations/decoration_data.gd
@@ -0,0 +1,6 @@
+class_name DecorationData extends Resource;
+
+@export var name: StringName = &"";
+@export var texture: Texture2D;
+@export var cost_to_build: int = 50;
+@export var attractiveness = 5;
